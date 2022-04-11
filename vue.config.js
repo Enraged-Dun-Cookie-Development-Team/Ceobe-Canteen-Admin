7f7cf77e68866061348f1644b0da7e07499652eb
lwt-414 <2957035701@qq.com>
2022-04-11 14:57:46 +0800

    🚨 修复build空白界面的情况

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   // lintOnSave: process.env.NODE_ENV === 'development',
@@ -37,7 +37,7 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    before: requinpmre('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
