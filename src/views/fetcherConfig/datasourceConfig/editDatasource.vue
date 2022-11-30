e40d76dc9f50e84fb48b443b4e77e7956ccc7d25
phidiaLam <2957035701@qq.com>
2022-11-30 17:51:19 +0800

    💄 配置数据源弹出界面

---
+++ src/views/fetcherConfig/datasourceConfig/editDatasource.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-drawer
+    :visible.sync="showDraw"
+    title="编辑数据源"
+    direction="rtl"
+    size="40%"
+    custom-class="drawer-max-height"
+    @close="onClose"
+  >
+    <div class="edit-area pr-24 pl-24">
+    </div>
+  </el-drawer>
+</template>
+
+<script>
+export default {
+    name: "EditDatasource",
+    data() {
+        return {
+            showDraw: true,
+        };
+    },
+    methods: {
+        onClose() {
+
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.pr-24 {
+  padding-right: 24px;
+}
+
+.pl-24 {
+  padding-left: 24px;
+}
+</style>
