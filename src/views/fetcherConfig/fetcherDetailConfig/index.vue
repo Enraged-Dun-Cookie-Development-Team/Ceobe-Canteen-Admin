c070bc82d8d6c454c6adcd33bb6a51d5f228feea
kazeliu <kaze.liu@qq.com>
2022-11-30 16:00:54 +0800

    :green_heart: 添加时间频率的调整

--- src/views/fetcherConfig/fetcherDetailConfig/index.vue
+++ src/views/fetcherConfig/fetcherDetailConfig/index.vue
@@ -71,20 +71,33 @@
                   >
                     <!--这里是卡片的内容，如果你要在里面加字段，请先在groupAddDatasource方法内初始化的时候添加相应字段以免无法监听-->
                     <div>
-                      <i class="mb-10">{{ datasource.name }}</i>
+                      <div class="mb-10">
+                        <i class="el-icon-s-order mr-5"></i>{{ datasource.name }}
+                      </div>
+                      <div
+                        class="mh-10 cursor-pointer"
+                        @click="getIntervalByTimeRangeAndShowWindow(datasource,servers.number,groupIndex,datasourceIndex)"
+                      >
+                        非规定时间段的常规频率：<b style="color: #23ADE5">{{ datasource.time?datasource.time:0 }}</b>毫秒
+                      </div>
+                      <div
+                        class="cursor-pointer"
+                        @click="getIntervalByTimeRangeAndShowWindow(datasource,servers.number,groupIndex,datasourceIndex)"
+                        v-html="datasource.intervalByTimeRange.length>0?getIntervalByTimeRangeString(datasource.intervalByTimeRange):'点击这里修改频率'"
+                      >
+                      </div>
                       <br />
-                      <el-input
-                        v-model="datasource.time" class="mh-10"
-                        placeholder="我也不知道你要输入什么，反正就放在这里了"
-                      />
                       <i v-if="datasource.datasource.length == 0" style="color: #848488">拖拽tag把来源添加到这里</i>
                       <div v-else>
                         <el-tag
                           v-for="sourceTypeName in datasource.datasource"
                           :key="sourceTypeName.name"
-                          class="mv-10"
+                          class="mv-10 cursor-pointer"
                           closable
-                          @close="removeSource(sourceTypeName.name,servers.number,groupIndex,datasourceIndex)"
+                          :title="sourceTypeName.arg.ignoreEmpty?'当前是未启用的状态':''"
+                          :type="sourceTypeName.arg.ignoreEmpty?'info':''"
+                          @close="removeSource(sourceTypeName,servers.number,groupIndex,datasourceIndex)"
+                          @click="changeIgnoreEmptyStatus(sourceTypeName,servers.number,groupIndex,datasourceIndex)"
                         >
                           {{ sourceTypeName.name }}
                         </el-tag>
@@ -106,6 +119,57 @@
       autosize
       placeholder="请输入内容"
     />
+    <el-dialog
+      :visible.sync="timePickerWindowInfo.show"
+      width="450px"
+      :title="timePickerWindowInfo.title"
+    >
+      <el-input
+        v-model="timePickerWindowInfo.time"
+        class="mh-10"
+        placeholder="时间段外的常规频率"
+      />
+      <div
+        v-for="(item,index) in timePicker" :key="'timePicker'+index"
+        class="flex-between mh-10"
+      >
+        <el-time-select
+          v-model="timePicker[index].startTime"
+          :picker-options="{
+            step: '00:15',
+            start:'00:00',
+            end:'24:00'
+          }"
+          style="flex: 1"
+          placeholder="开始时间"
+        />
+        <el-time-select
+          v-model="timePicker[index].endTime"
+          :picker-options="{
+            step: '00:15',
+            start:'00:00',
+            end:'24:00'
+          }"
+          style="flex: 1"
+          class="ml-5"
+          placeholder="结束时间"
+        />
+        <el-input
+          v-model="timePicker[index].interval"
+          style="flex:1"
+          class="ml-5"
+          placeholder="蹲饼轮询毫秒"
+        />
+      </div>
+      <div class="flex flex-between mv-10 mh-10">
+        <el-button @click="addIntervalByTimeRangeLine">
+          添加一行自定义时间段频率
+        </el-button>
+        <el-button @click="addIntervalByTimeRangeToGroups">
+          确认
+        </el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -118,8 +182,16 @@ export default {
             serverLiveList: [], // 生成数数组
             sourceTypeList: [], // 类别
             sourceTypeNameList: [], // 类别下的账号
-            dragItem: {},
-            removeItem: {},
+            dragItem: {}, // 拖拽对象
+            timePickerWindowInfo: {
+                show: false,
+                serversIndex: null,
+                groupIndex: null,
+                datasourceIndex: null,
+                title: "",
+                time: null,
+            },// 打开的弹窗内保存的信息
+            timePicker: [{}],// 时间列表
         };
     },
     computed: {
@@ -144,7 +216,7 @@ export default {
                     {
                         name: '官方账号',
                         type: 'bilibili',
-                        arg: { uid: '161775300' }
+                        arg: { uid: '161775300', }
                     },
                     {
                         name: '明日方舟终末地',
@@ -198,33 +270,37 @@ export default {
                 findData.groups.push({
                     name: value,
                     datasource: [],
-                    time:null
+                    time: null,
+                    intervalByTimeRange: []
                 });
             }).catch(() => {
 
             });
         },
+        // 获取平台信息 bilibili 微博 等
         checkSourceType(name) {
             this.nextPage();
             this.getSourceTypeNameList(name);
         },
-
+        // 跳转到下一操作步骤
         nextPage() {
             this.stepIndex++;
             if (this.stepIndex > 3) {
                 this.stepIndex = 3;
             }
         },
+        // 跳转回上一操作步骤
         prevPage() {
             this.stepIndex--;
             if (this.stepIndex < 1) {
                 this.stepIndex = 1;
             }
         },
-
+        // 拖拽的条目的信息
         setDragItem(item) {
             this.dragItem = item;
         },
+        // 总对象内添加蹲饼源
         addSourceInDatasource(event) {
             event.preventDefault();
             if (event.currentTarget.classList.contains('el-card')) {
@@ -239,10 +315,70 @@ export default {
             }
             this.dragItem = null;
         },
+        // 点击标签的x删除总对象内的蹲饼源
         removeSource(sourceTypeName, serversIndex, groupIndex, datasourceIndex) {
             let datasource = this.serverLiveList.find(x => x.number == serversIndex).server[groupIndex].groups[datasourceIndex];
-            let index = datasource.datasource.findIndex(x => x.name == sourceTypeName);
+            let index = datasource.datasource.findIndex(x => x.name == sourceTypeName.name);
             datasource.datasource.splice(index, 1);
+        },
+        // 修改源状态（ignoreEmpty）的true和false
+        changeIgnoreEmptyStatus(sourceTypeName, serversIndex, groupIndex, datasourceIndex) {
+            let datasource = this.serverLiveList.find(x => x.number == serversIndex).server[groupIndex].groups[datasourceIndex];
+            let source = datasource.datasource.find(x => x.name == sourceTypeName.name);
+            if (source.arg && Object.prototype.hasOwnProperty.call(source.arg, "ignoreEmpty")) {
+                this.$set(source.arg, 'ignoreEmpty', !source.arg.ignoreEmpty);
+            } else {
+                this.$set(source.arg, 'ignoreEmpty', true);
+            }
+        },
+        // 时间添加一行
+        addIntervalByTimeRangeLine() {
+            this.timePicker.push({});
+        },
+        // 打开蹲饼时间段频率功能的窗口
+        getIntervalByTimeRangeAndShowWindow(datasource, serversIndex, groupIndex, datasourceIndex) {
+            this.timePickerWindowInfo = {
+                show: true,
+                serversIndex,
+                groupIndex,
+                datasourceIndex,
+                title: '添加 ' + datasource.name + ' 的蹲饼频率',
+                time:datasource.time
+            };
+            this.timePicker = datasource.intervalByTimeRange.map(x=>{
+                return { startTime:x.timeRange[0],
+                    endTime:x.timeRange[1],
+                    interval:x.interval };
+            });
+        },
+        // 把设置好得蹲饼时间段频率功能添加到总对象字符串内
+        addIntervalByTimeRangeToGroups() {
+            let datasource = this.serverLiveList.find(x => x.number == this.timePickerWindowInfo.serversIndex).server[this.timePickerWindowInfo.groupIndex].groups[this.timePickerWindowInfo.datasourceIndex];
+            datasource.intervalByTimeRange = this.timePicker.map(x => {
+                return {
+                    timeRange: [x.startTime, x.endTime],
+                    interval: x.interval
+                };
+            });
+            datasource.time = this.timePickerWindowInfo.time;
+            this.timePickerWindowInfo= {
+                show: false,
+                serversIndex: null,
+                groupIndex: null,
+                datasourceIndex: null,
+                title: "",
+                time: null,
+            };
+            this.timePicker=[{}];
+        },
+        // 把时间段内蹲饼频率的设置转化为HTML显示
+        getIntervalByTimeRangeString(data) {
+            let html = `<div class="interval-by-time-range-html"><span>共${data.length}条:</span>`;
+            html += data.map((x, index) => {
+                return `<span>第<b>${index + 1}</b>条:<b>${x.timeRange[0]}</b>到<b>${x.timeRange[0]}</b>,频率为<b>${x.interval}</b>毫秒;</span>`;
+            });
+            html += '</div>';
+            return html;
         }
     }
 };
@@ -250,6 +386,14 @@ export default {
 
 <style lang="scss">
 .server-source-import-json {
+
+  .ml-5 {
+    margin-left: 5px;
+  }
+
+  .mr-5 {
+    margin-right: 5px;
+  }
 
   .mb-10 {
     margin-bottom: 10px;
@@ -265,6 +409,10 @@ export default {
 
   .mh-10 {
     margin: 10px 0 10px 0;
+  }
+
+  .cursor-pointer {
+    cursor: pointer;
   }
 
   .flex-center {
@@ -288,6 +436,13 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
+  }
+
+  .interval-by-time-range-html {
+
+    b {
+      color: #23ADE5;
+    }
   }
 }
 </style>
