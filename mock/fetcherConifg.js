78439def1c33a01fbe4108ee48aa4fe18f0874c6
phidialam <2957035701@qq.com>
2023-01-01 16:10:42 +0800

    🤡 根据修改改动mock数据

--- mock/fetcherConifg.js
+++ mock/fetcherConifg.js
@@ -338,26 +338,31 @@ module.exports = [
                     {
                         id: 0,
                         nickname: "明日方舟-B站",
+                        datasource: "bilibili",
                         config: { uid: '161775300' }
                     },
                     {
                         id: 1,
                         nickname: "明日方舟终末地-B站",
+                        datasource: "bilibili",
                         config: { uid: '1265652806' },
                     },
                     {
                         id: 2,
                         nickname: "来自星尘-B站",
+                        datasource: "bilibili",
                         config: { uid: '1883857209' },
                     },
                     {
                         id: 3,
                         nickname: "重力井动画-B站",
+                        datasource: "bilibili",
                         config: { uid: '1554642444' },
                     },
                     {
                         id: 4,
                         nickname: "CubesCollective-B站",
+                        datasource: "bilibili",
                         config: { uid: '2123591088' },
                     }
                 ],
@@ -365,6 +370,7 @@ module.exports = [
                     {
                         id: 5,
                         nickname: "明日方舟Arknights-微博",
+                        datasource: "weibo",
                         config: {
                             uid: '6279793937',
                             requestOptions: {
@@ -379,31 +385,37 @@ module.exports = [
                     {
                         id: 6,
                         nickname: "一拾山-微博",
+                        datasource: "weibo",
                         config: { uid: '7506039414' },
                     },
                     {
                         id: 7,
                         nickname: "明日方舟朝陇山-微博",
+                        datasource: "weibo",
                         config: { uid: '6441489862' },
                     },
                     {
                         id: 8,
                         nickname: "泰拉记事社-微博",
+                        datasource: "weibo",
                         config: { uid: '7499841383' },
                     },
                     {
                         id: 9,
                         nickname: "鹰角网络-微博",
+                        datasource: "weibo",
                         config: { uid: '7461423907' },
                     },
                     {
                         id: 10,
                         nickname: "乌柯塔界限OCTAVEDGE-微博",
+                        datasource: "weibo",
                         config: { uid: '7499841383' },
                     },
                     {
                         id: 11,
                         nickname: "明日方舟终末地-微博",
+                        datasource: "weibo",
                         config: { uid: '7745672941' },
                     }
                 ],
@@ -411,11 +423,13 @@ module.exports = [
                     {
                         id: 12,
                         nickname: "塞壬唱片-MSR-网易云",
+                        datasource: "netease-cloud-musis",
                         config: { artistId: '32540734' },
                     },
                     {
                         id: 13,
                         nickname: "塞壬唱片-MSR-Cubes Collective",
+                        datasource: "netease-cloud-musis",
                         config: { artistId: '50653540' },
                     }
                 ],
@@ -423,11 +437,13 @@ module.exports = [
                     {
                         id: 14,
                         nickname: "游戏内公告(安卓)-明日方舟",
+                        datasource: "arknights/monster-siren",
                         config: { platform: 'Android' },
                     },
                     {
                         id: 15,
                         nickname: "游戏版本(安卓)-明日方舟",
+                        datasource: "arknights/game-version",
                         config: { platform: 'Android' },
                     },
                 ],
@@ -435,16 +451,19 @@ module.exports = [
                     {
                         id: 16,
                         nickname: "塞壬唱片官网-明日方舟",
+                        datasource: "arknights/monster-siren",
                         config: { },
                     },
                     {
                         id: 17,
                         nickname: "官网-明日方舟",
+                        datasource: "arknights/official-website",
                         config: { },
                     },
                     {
                         id: 18,
                         nickname: "泰拉记事社官网-明日方舟",
+                        datasource: "arknights/terra-historicus",
                         config: { },
                     },
                 ]
@@ -466,11 +485,11 @@ module.exports = [
             const { type_id } = req.query;
 
             let data = {
-                "bilibili": [{ "number":1,"server":[{ "groups":[{ "name":"test1","type":"bilibili","datasource":[0,1],"interval":3000 },{ "name":"test2","type":"bilibili","datasource":[2,3] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"test3","type":"bilibili","datasource":[0,1,2,3],"interval_by_time_range":[{ "time_range":["04:00","06:00"],"interval":0 }] }] },{ "groups":[{ "name":"test4","type":"bilibili","datasource":[4],"interval":3000 }] }] },{ "number":3,"server":[{ "groups":[{ "name":"test4","type":"bilibili","datasource":[0,1,2] },{ "name":"test5","type":"bilibili","datasource":[3] }] },{ "groups":[{ "name":"test6","type":"bilibili","datasource":[4] }] },{ "groups":[] }] },{ "number":4,"server":[{ "groups":[{ "name":"test7","type":"bilibili","datasource":[0,4] }] },{ "groups":[{ "name":"test8","type":"bilibili","datasource":[1] }] },{ "groups":[{ "name":"test9","type":"bilibili","datasource":[2] }] },{ "groups":[{ "name":"test10","type":"bilibili","datasource":[3] }] }] }],
-                "weibo": [{ "number":1,"server":[{ "groups":[{ "name":"weibo1","type":"weibo","datasource":[5,11] },{ "name":"weibo2","type":"weibo","datasource":[6,7,8,10,9] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"weibo3","type":"weibo","datasource":[6,7,8,9,10] }] },{ "groups":[{ "name":"微博4","type":"weibo","datasource":[11] },{ "name":"微博5","type":"weibo","datasource":[5] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"微博6","type":"weibo","datasource":[5,6,10] }] },{ "groups":[{ "name":"微博7","type":"weibo","datasource":[7,8,11] }] },{ "groups":[{ "name":"微博8","type":"weibo","datasource":[9] }] }] },{ "number":4,"server":[{ "groups":[{ "name":"微博9","type":"weibo","datasource":[5,6] }] },{ "groups":[{ "name":"微博10","type":"weibo","datasource":[7,9] }] },{ "groups":[{ "name":"微博11","type":"weibo","datasource":[10] }] },{ "groups":[{ "name":"微博12","type":"weibo","datasource":[11,8] }] }] },{ "number":5,"server":[{ "groups":[{ "name":"微博13","type":"weibo","datasource":[11,5,7] }] },{ "groups":[{ "name":"微博17","type":"weibo","datasource":[8] }] },{ "groups":[{ "name":"微博16","type":"weibo","datasource":[10] }] },{ "groups":[{ "name":"微博15","type":"weibo","datasource":[6] }] },{ "groups":[{ "name":"微博14","type":"weibo","datasource":[9] }] }] }],
-                "netease-cloud-music": [{ "number":1,"server":[{ "groups":[{ "name":"网易云1","type":"netease-cloud-music","datasource":[12,13] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"网易云2","type":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云3","type":"netease-cloud-music","datasource":[13] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"网易云4","type":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云5","type":"netease-cloud-music","datasource":[13] }] },{ "groups":[] }] },{ "number":4,"server":[{ "groups":[{ "name":"网易云6","type":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云7","type":"netease-cloud-music","datasource":[13] }] },{ "groups":[] },{ "groups":[] }] },{ "number":5,"server":[{ "groups":[{ "name":"网易云8","type":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云9","type":"netease-cloud-music","datasource":[13] }] },{ "groups":[] },{ "groups":[] },{ "groups":[] }] }],
-                "arknights-game": [{ "number":1,"server":[{ "groups":[{ "name":"game1","type":"arknights-game","datasource":[14] },{ "name":"game2","type":"arknights-game","datasource":[15] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"game3","type":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game4","type":"arknights-game","datasource":[15] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"game5","type":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game6","type":"arknights-game","datasource":[15] }] },{ "groups":[] }] },{ "number":4,"server":[{ "groups":[{ "name":"game7","type":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game8","type":"arknights-game","datasource":[15] }] },{ "groups":[] },{ "groups":[] }] },{ "number":5,"server":[{ "groups":[{ "name":"game9","type":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game10","type":"arknights-game","datasource":[15] }] },{ "groups":[] },{ "groups":[] },{ "groups":[] }] }],
-                "arknights-website": [{ "number":1,"server":[{ "groups":[{ "name":"web4","type":"arknights-website","datasource":[16,17,18] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"web5","type":"arknights-website","datasource":[16,17] }] },{ "groups":[{ "name":"web6","type":"arknights-website","datasource":[18] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"web7","type":"arknights-website","datasource":[16] }] },{ "groups":[{ "name":"web8","type":"arknights-website","datasource":[17] }] },{ "groups":[{ "name":"web9","type":"arknights-website","datasource":[18] }] }] },{ "number":4,"server":[{ "groups":[{ "name":"web10","type":"arknights-website","datasource":[16] }] },{ "groups":[{ "name":"web11","type":"arknights-website","datasource":[17] }] },{ "groups":[{ "name":"web12","type":"arknights-website","datasource":[18] }] },{ "groups":[] }] },{ "number":5,"server":[{ "groups":[{ "name":"web1","type":"arknights-website","datasource":[16] }] },{ "groups":[{ "name":"web2","type":"arknights-website","datasource":[17] }] },{ "groups":[{ "name":"web3","type":"arknights-website","datasource":[18] }] },{ "groups":[] },{ "groups":[] }] }],
+                "bilibili": [{ "number":1,"server":[{ "groups":[{ "name":"test1","platform":"bilibili","datasource":[0,1],"interval":3000 },{ "name":"test2","platform":"bilibili","datasource":[2,3] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"test3","platform":"bilibili","datasource":[0,1,2,3],"interval_by_time_range":[{ "time_range":["04:00","06:00"],"interval":0 }] }] },{ "groups":[{ "name":"test4","platform":"bilibili","datasource":[4],"interval":3000 }] }] },{ "number":3,"server":[{ "groups":[{ "name":"test4","platform":"bilibili","datasource":[0,1,2] },{ "name":"test5","platform":"bilibili","datasource":[3] }] },{ "groups":[{ "name":"test6","platform":"bilibili","datasource":[4] }] },{ "groups":[] }] },{ "number":4,"server":[{ "groups":[{ "name":"test7","platform":"bilibili","datasource":[0,4] }] },{ "groups":[{ "name":"test8","platform":"bilibili","datasource":[1] }] },{ "groups":[{ "name":"test9","platform":"bilibili","datasource":[2] }] },{ "groups":[{ "name":"test10","platform":"bilibili","datasource":[3] }] }] }],
+                "weibo": [{ "number":1,"server":[{ "groups":[{ "name":"weibo1","platform":"weibo","datasource":[5,11] },{ "name":"weibo2","platform":"weibo","datasource":[6,7,8,10,9] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"weibo3","platform":"weibo","datasource":[6,7,8,9,10] }] },{ "groups":[{ "name":"微博4","platform":"weibo","datasource":[11] },{ "name":"微博5","platform":"weibo","datasource":[5] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"微博6","platform":"weibo","datasource":[5,6,10] }] },{ "groups":[{ "name":"微博7","platform":"weibo","datasource":[7,8,11] }] },{ "groups":[{ "name":"微博8","platform":"weibo","datasource":[9] }] }] },{ "number":4,"server":[{ "groups":[{ "name":"微博9","platform":"weibo","datasource":[5,6] }] },{ "groups":[{ "name":"微博10","platform":"weibo","datasource":[7,9] }] },{ "groups":[{ "name":"微博11","platform":"weibo","datasource":[10] }] },{ "groups":[{ "name":"微博12","platform":"weibo","datasource":[11,8] }] }] },{ "number":5,"server":[{ "groups":[{ "name":"微博13","platform":"weibo","datasource":[11,5,7] }] },{ "groups":[{ "name":"微博17","platform":"weibo","datasource":[8] }] },{ "groups":[{ "name":"微博16","platform":"weibo","datasource":[10] }] },{ "groups":[{ "name":"微博15","platform":"weibo","datasource":[6] }] },{ "groups":[{ "name":"微博14","platform":"weibo","datasource":[9] }] }] }],
+                "netease-cloud-music": [{ "number":1,"server":[{ "groups":[{ "name":"网易云1","platform":"netease-cloud-music","datasource":[12,13] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"网易云2","platform":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云3","platform":"netease-cloud-music","datasource":[13] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"网易云4","platform":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云5","platform":"netease-cloud-music","datasource":[13] }] },{ "groups":[] }] },{ "number":4,"server":[{ "groups":[{ "name":"网易云6","platform":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云7","platform":"netease-cloud-music","datasource":[13] }] },{ "groups":[] },{ "groups":[] }] },{ "number":5,"server":[{ "groups":[{ "name":"网易云8","platform":"netease-cloud-music","datasource":[12] }] },{ "groups":[{ "name":"网易云9","platform":"netease-cloud-music","datasource":[13] }] },{ "groups":[] },{ "groups":[] },{ "groups":[] }] }],
+                "arknights-game": [{ "number":1,"server":[{ "groups":[{ "name":"game1","platform":"arknights-game","datasource":[14] },{ "name":"game2","platform":"arknights-game","datasource":[15] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"game3","platform":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game4","platform":"arknights-game","datasource":[15] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"game5","platform":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game6","platform":"arknights-game","datasource":[15] }] },{ "groups":[] }] },{ "number":4,"server":[{ "groups":[{ "name":"game7","platform":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game8","platform":"arknights-game","datasource":[15] }] },{ "groups":[] },{ "groups":[] }] },{ "number":5,"server":[{ "groups":[{ "name":"game9","platform":"arknights-game","datasource":[14] }] },{ "groups":[{ "name":"game10","platform":"arknights-game","datasource":[15] }] },{ "groups":[] },{ "groups":[] },{ "groups":[] }] }],
+                "arknights-website": [{ "number":1,"server":[{ "groups":[{ "name":"web4","platform":"arknights-website","datasource":[16,17,18] }] }] },{ "number":2,"server":[{ "groups":[{ "name":"web5","platform":"arknights-website","datasource":[16,17] }] },{ "groups":[{ "name":"web6","platform":"arknights-website","datasource":[18] }] }] },{ "number":3,"server":[{ "groups":[{ "name":"web7","platform":"arknights-website","datasource":[16] }] },{ "groups":[{ "name":"web8","platform":"arknights-website","datasource":[17] }] },{ "groups":[{ "name":"web9","platform":"arknights-website","datasource":[18] }] }] },{ "number":4,"server":[{ "groups":[{ "name":"web10","platform":"arknights-website","datasource":[16] }] },{ "groups":[{ "name":"web11","platform":"arknights-website","datasource":[17] }] },{ "groups":[{ "name":"web12","platform":"arknights-website","datasource":[18] }] },{ "groups":[] }] },{ "number":5,"server":[{ "groups":[{ "name":"web1","platform":"arknights-website","datasource":[16] }] },{ "groups":[{ "name":"web2","platform":"arknights-website","datasource":[17] }] },{ "groups":[{ "name":"web3","platform":"arknights-website","datasource":[18] }] },{ "groups":[] },{ "groups":[] }] }],
             };
 
 
