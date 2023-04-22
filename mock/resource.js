86eba669e859e0e0a5e5b3b1961a4996902ddb45
phidialam <2957035701@qq.com>
2023-04-22 14:50:24 +0800

    👔 资源管理处添加倒计时类型

--- mock/resource.js
+++ mock/resource.js
@@ -14,6 +14,7 @@ module.exports = [
                     {
                         "text": "当前轮换池结束",
                         "remark": "刻俄柏[兑换],水月,赤冬,莱恩哈特[兑换],安哲拉",
+                        "countdown_type": null,
                         "time": "2022-02-17 03:59:59",
                         "start_time": "2022-02-03 04:00:00",
                         "over_time": "2022-02-17 03:59:59"
@@ -21,6 +22,7 @@ module.exports = [
                     {
                         "text": "当前轮换池结束",
                         "remark": "棘刺[兑换]、艾雅法拉、乌有 、诗怀雅[兑换]、雷蛇",
+                        "countdown_type": null,
                         "time": "2022-02-03 03:59:59",
                         "start_time": "2022-01-20 04:00:00",
                         "over_time": "2022-02-03 03:59:59"
@@ -29,6 +31,7 @@ module.exports = [
                     {
                         "text": "下个活动池开启",
                         "remark": "令[限定]，老鲤，夜半",
+                        "countdown_type": "banner",
                         "time": "2022-01-14 15:59:59",
                         "start_time": "2022-01-14 04:00:00",
                         "over_time": "2022-01-14 15:59:59"
@@ -36,6 +39,7 @@ module.exports = [
                     {
                         "text": "下个活动池开启",
                         "remark": "令[限定]，老鲤，夜半",
+                        "countdown_type": "banner",
                         "time": "2022-01-25 15:59:59",
                         "start_time": "2022-01-15 04:00:00",
                         "over_time": "2022-01-25 15:59:59"
@@ -43,6 +47,7 @@ module.exports = [
                     {
                         "text": "当前活动池结束",
                         "remark": "令[限定]，老鲤，夜半",
+                        "countdown_type": "banner",
                         "time": "2022-02-08 03:59:59",
                         "start_time": "2022-01-25 16:00:00",
                         "over_time": "2022-02-08 03:59:59"
@@ -51,6 +56,7 @@ module.exports = [
                     {
                         "text": "SideStory「将进酒」，活动开启",
                         "remark": "解锁条件：通关主线1-10",
+                        "countdown_type": "activity",
                         "time": "2022-01-25 15:59:59",
                         "start_time": "2022-01-20 04:00:00",
                         "over_time": "2022-01-25 15:59:59"
@@ -58,6 +64,7 @@ module.exports = [
                     {
                         "text": "SideStory「画中人」，复刻结束",
                         "remark": "解锁条件：通关主线1-10",
+                        "countdown_type": "activity",
                         "time": "2022-01-20 03:59:59",
                         "start_time": "2022-01-10 16:00:00",
                         "over_time": "2022-01-20 03:59:59"
@@ -65,6 +72,7 @@ module.exports = [
                     {
                         "text": "SideStory「将进酒」，活动结束",
                         "remark": "通关主线1-10",
+                        "countdown_type": "activity",
                         "time": "2022-02-08 03:59:59",
                         "start_time": "2022-01-25 16:00:00",
                         "over_time": "2022-02-08 03:59:59"
