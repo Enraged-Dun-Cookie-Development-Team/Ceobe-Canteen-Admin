ce46746f8bc63c23cb595eabc7c4f3ac6e6161f2
phidiaLam <2957035701@qq.com>
2023-07-29 23:26:32 +0800

    👔 调整手机端版本界面

--- src/views/canteen/phoneVersion/index.vue
+++ src/views/canteen/phoneVersion/index.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="mainWindow">
-    <h3>插件端版本信息</h3>
+    <h3>手机端版本信息</h3>
     <el-form
       ref="phoneForm"
       :model="phoneForm"
       :rules="phoneRules"
       label-position="left"
-      label-width="110px"
+      label-width="150px"
     >
       <el-form-item label="版本号" prop="version">
         <el-input
           v-model="phoneForm.version"
           placeholder="请输入版本号"
+          @blur="changeVersion"
         />
       </el-form-item>
       <el-form-item label="强制更新" prop="force">
@@ -29,6 +30,30 @@
           type="textarea"
           :rows="8"
           placeholder="请输入描述"
+        />
+      </el-form-item>
+      <el-form-item label="apk下载链接" prop="apk">
+        <el-input
+          v-model="phoneForm.apk"
+          placeholder="请输入apk下载链接"
+        />
+      </el-form-item>
+      <el-form-item label="apk备用下载链接" prop="spare_apk">
+        <el-input
+          v-model="phoneForm.spare_apk"
+          placeholder="请输入apk备用下载链接"
+        />
+      </el-form-item>
+      <el-form-item label="百度云" prop="baidu">
+        <el-input
+          v-model="phoneForm.baidu"
+          placeholder="请输入百度云链接"
+        />
+      </el-form-item>
+      <el-form-item label="百度云描述" prop="baidu_text">
+        <el-input
+          v-model="phoneForm.baidu_text"
+          placeholder="请输入百度云描述"
         />
       </el-form-item>
     </el-form>
@@ -56,6 +81,10 @@ export default {
                 force: true,
                 last_force_version: "",
                 description: "",
+                apk: "",
+                spare_apk: "",
+                baidu: "https://pan.baidu.com/s/1kzY6kpfYqLcGpuaiwQOGoA",
+                baidu_text: "备用下载（提取码 jzq9）",
             },
             phoneRules: {
                 version: [
@@ -96,6 +125,34 @@ export default {
                         trigger: "blur",
                     },
                 ],
+                apk: [
+                    {
+                        required: true,
+                        message: "不许空不许空",
+                        trigger: ["change", "blur"],
+                    },
+                ],
+                spare_apk: [
+                    {
+                        required: true,
+                        message: "不许空不许空!!",
+                        trigger: ["change", "blur"],
+                    },
+                ],
+                baidu: [
+                    {
+                        required: true,
+                        message: "都说了不要为空了啦",
+                        trigger: ["change", "blur"],
+                    },
+                ],
+                baidu_text: [
+                    {
+                        required: true,
+                        message: "都说了不要为空了啦",
+                        trigger: ["change", "blur"],
+                    },
+                ],
             },
         };
     },
@@ -119,6 +176,10 @@ export default {
                         });
                 }
             });
+        },
+        changeVersion() {
+            this.phoneForm.apk = `https://github.com/Enraged-Dun-Cookie-Development-Team/Dun-Cookie-Flutter/releases/download/${this.phoneForm.version}/Ceobe-Canteen-${this.phoneForm.version}.apk`;
+            this.phoneForm.spare_apk = `https://kgithub.com/Enraged-Dun-Cookie-Development-Team/Dun-Cookie-Flutter/releases/download/${this.phoneForm.version}/Ceobe-Canteen-${this.phoneForm.version}.apk`;
         },
     },
 };
